ea4d819f78042157b9be5e4e60ffec74820f2a47
Gauravkatnoria12 <144324982+Gauravkatnoria12@users.noreply.github.com>
2026-04-13 19:56:48 +0530

    Game-Edited

--- Small Programs/Rock_Paper_Scissers.py
+++ Small Programs/Rock_Paper_Scissers.py
@@ -9,27 +9,28 @@ user_choice = int(input("Enter your choice (1, 2, or 3): "))
 moves = [1, 2, 3]
 computer_move = random.choice(moves)
 
-if user_choice in [1, 2, 3]:
-    if computer_move == 1 and user_choice == 1 :
-        print("Tie! Computer chose Rock, you chose Rock")
-    elif computer_move == 1 and user_choice == 2 :
-        print("You win! Computer chose Rock, you chose Paper")
-    elif computer_move == 1 and user_choice == 3 :
-        print("You lose! Computer chose Rock, you chose Scissors")
-    elif computer_move == 2 and user_choice == 1 :
-        print("You lose! Computer chose Paper, you chose Rock")
-    elif computer_move == 2 and user_choice == 2 :
-        print("Tie! Computer chose Paper, you chose Paper")
-    elif computer_move == 2 and user_choice == 3 :
-        print("You win! Computer chose Paper, you chose Scissors")
-    elif computer_move == 3 and user_choice == 1 :
-        print("You win! Computer chose Scissors, you chose Rock")
-    elif computer_move == 3 and user_choice == 2 :
-        print("You lose! Computer chose Scissors, you chose Paper")
-    elif computer_move == 3 and user_choice == 3 :
-        print("Tie! Computer chose Scissors, you chose Scissors")
-    else :
+while 1:
+    if user_choice in [1, 2, 3]:
+        if computer_move == 1 and user_choice == 1 :
+            print("Tie! Computer chose Rock, you chose Rock")
+        elif computer_move == 1 and user_choice == 2 :
+            print("You win! Computer chose Rock, you chose Paper")
+        elif computer_move == 1 and user_choice == 3 :
+            print("You lose! Computer chose Rock, you chose Scissors")
+        elif computer_move == 2 and user_choice == 1 :
+            print("You lose! Computer chose Paper, you chose Rock")
+        elif computer_move == 2 and user_choice == 2 :
+            print("Tie! Computer chose Paper, you chose Paper")
+        elif computer_move == 2 and user_choice == 3 :
+            print("You win! Computer chose Paper, you chose Scissors")
+        elif computer_move == 3 and user_choice == 1 :
+            print("You win! Computer chose Scissors, you chose Rock")
+        elif computer_move == 3 and user_choice == 2 :
+            print("You lose! Computer chose Scissors, you chose Paper")
+        elif computer_move == 3 and user_choice == 3 :
+            print("Tie! Computer chose Scissors, you chose Scissors")
+        else :
+            print("Invalid choice! Please enter 1, 2, or 3.")
+    else:
         print("Invalid choice! Please enter 1, 2, or 3.")
-else:
-    print("Invalid choice! Please enter 1, 2, or 3.")
 

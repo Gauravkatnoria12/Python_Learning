9073f56d68b3570c5cca9e00f0378a49136b57a8
Gauravkatnoria12 <g200007k@gmail.com>
2026-06-12 15:05:45 +0530

    problem_solving

---
+++ Problems_Solving/Problem_1.py
@@ -0,0 +1,10 @@
+try:
+    num = int(input("Enter a number: "))
+
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+
+except ValueError:
+    print("Please enter a valid integer.")
